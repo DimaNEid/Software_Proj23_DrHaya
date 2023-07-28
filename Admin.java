c258aeaff104d5a1cb46116fb3db7d2189ac4ac3
RoaaAboLibdeh <137789717+RoaaAboLibdeh@users.noreply.github.com>
2023-07-28 19:00:34 +0200

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Admin {
+    public String getEmail() {
+        return "AdminggRoDi@gmail.com";
+    }
+    public String getAdminId() {
+        return "123432";
+    }
+    public String getAdminPassword() {
+        return "315SV#H";
+    }
+
+    public String getEmailPassword() {
+        return "rodi123456789";
+    }
+}
