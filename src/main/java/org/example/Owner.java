56e8709438da4db121a1835d3a65c727e201172f
DimaNEid <140755066+DimaNEid@users.noreply.github.com>
2023-07-28 22:29:41 +0300

    Update Owner.java

--- src/main/java/org/example/Owner.java
+++ src/main/java/org/example/Owner.java
@@ -5,9 +5,31 @@ import java.util.List;
 
 public class Owner extends User{
 
+    private String address;
+    private String phone;
+
+    public String getPhone(){return phone;}
+
+    public void setPhone(String phone){this.phone=phone;}
+
+    public String getAddress() {return address;}
+
+    public void setAddress(String address) {this.address = address;}
+
+
     public List<Residence> residences = new ArrayList<>();
-    public Owner(String id, String name, String username, String password) {
+    public Owner(String id, String name, String username, String password, String address, String phone) {
         super(id, name, username, password);
+        this.address=address;
+        this.phone=phone;
+    }
+
+    @Override
+
+    public String toString(){
+        return "Owner{"+ super.toString() + '\'' +
+                ", address ='" + address + '\'' +
+                ", phone ='" + phone + '\'' + '}';
     }
 
 }
